754896818e1bc21deba09a6a655ab4fd9c6e02aa
Martin Nessa <martinnessa7@gmail.com>
2023-10-13 10:28:06 +0200

    testing azure deployment

--- src/main/java/com/martinnessa/firstSpringBoot/FirstSpringBootApplication.java
+++ src/main/java/com/martinnessa/firstSpringBoot/FirstSpringBootApplication.java
@@ -2,7 +2,7 @@ package com.martinnessa.firstSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Testing Azure deployment
+//Testing Azure deployment for first time
 
 @SpringBootApplication
 public class FirstSpringBootApplication {
